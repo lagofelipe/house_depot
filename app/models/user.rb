dc6dd1453564def6f73d6c80a14b8d55683f1031
lagofelipe <lagofelipe@gmail.com>
2020-11-09 21:26:36 -0600

    user model and root page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,19 @@ class User < ApplicationRecord
     has_many :orders
     has_many :order_items, through: :orders
     has_many :stores, through: :orders
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    def full_name
+     self.first_name + " " + self.last_name 
+    end
+
+    def orders_by_date
+     self.orders.sort_by{|orders|orders.created_at}.reverse
+    end
+    
+
+
 
 
 end
