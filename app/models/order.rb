7df215c4cac2381763959559df6a1186cad42d01
sarabastian <seb118@georgetown.edu>
2020-11-12 12:01:56 -0500

    migrations files included

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,16 +4,16 @@ class Order < ApplicationRecord
     has_many :order_items
     has_many :items, through: :order_items
 
-    accepts_nested_attributes_for :items
+    # accepts_nested_attributes_for :items
 
-    def items_attributes=(items_attributes)
-        item_attributes.values.each do |item_attribute|
-            if item_attribute["item_name"].present?
-          item = Item.find_or_create_by(item_attribute)
-          self.items << item
-            end
-        end
-    end
+    # def items_attributes=(items_attributes)
+    #     item_attributes.values.each do |item_attribute|
+    #         if item_attribute["item_name"].present?
+    #       item = Item.find_or_create_by(item_attribute)
+    #       self.items << item
+    #         end
+    #     end
+    # end
 
 
     def order_total
